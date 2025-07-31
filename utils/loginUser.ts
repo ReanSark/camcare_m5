43037f3f2d29ad7bdae1fff0d28dea0bb375bfb0
IIImarinIII <reansark@gmail.com>
2025-08-01 00:07:39 +0700

    - add and comment out a potential solution to the login issue

--- utils/loginUser.ts
+++ utils/loginUser.ts
@@ -5,9 +5,29 @@ import { account, databases } from "@/lib/appwrite.config";
 import { Query } from "appwrite";
 
 export const loginUser = async (email: string, password: string) => {
+
+  /* Try this later for the failed login issue*/
+  // try {
+  // await account.createEmailPasswordSession(email, password);
+  // const user = await account.get();
+
+  // console.log("User:", user);
+
+  // const roleDocs = await databases.listDocuments("camcare_db", "UserRoles", [
+  //   Query.equal("userId", user.$id),
+  // ]);
+
+  // const role = roleDocs.documents[0]?.role;
+  // console.log("Role:", role);
+
+  // // redirect logic...
+  // } catch (err: any) {
+  //   console.error("Full login error object:", err);
+  //   alert(err.message || "Login failed.");
+  // }
+
   
   await account.createEmailPasswordSession(email, password);
-
   const user = await account.get();
 
   const roleDocs = await databases.listDocuments("camcare_db", "UserRoles", [
